7c15716b5421f3bfadb2bc1bb5d69012a1d89d22
bogdan <lakatosh@phystech.edu>
2020-05-01 16:51:34 +0300

    passed not mine

--- 12_concurent_map/src/concurrent_map_2.cpp
+++ 12_concurent_map/src/concurrent_map_2.cpp
@@ -1,80 +1,110 @@
 #include "test_runner.h"
 #include "profile.h"
 
+#include <algorithm>
 #include <future>
 #include <mutex>
-#include <unordered_map>
-#include <vector>
-#include <utility>
-#include <algorithm>
+#include <shared_mutex>
 #include <random>
-#include <cmath>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <numeric>
 using namespace std;
 
-template <typename K, typename V, typename Hash = std::hash<K>>
-class ConcurrentMap {
+template<typename K, typename V, typename Hash = std::hash<K>>
+class ConcurrentMap
+{
 public:
-  using MapType = unordered_map<K, V, Hash>;
-  using KeyMap = unordered_map<K, size_t, Hash>;
+  using MyType = unordered_map<K, V, Hash>;
 
-  struct WriteAccess {
+  struct ProtectedMap
+  {
+    MyType map;
+    mutable shared_mutex mutex;
+  };
+
+  class WriteAccess
+  {
+  public:
+    WriteAccess(unique_lock<shared_mutex>&& lock, V& ref)
+      : lock_{ move(lock) }
+      , ref_to_value{ ref }
+    {}
+
     V& ref_to_value;
-    lock_guard<mutex> g;
-    WriteAccess(V& value, mutex& m): g(m), ref_to_value(value){}
-  };
 
-  struct ReadAccess {
+  private:
+    unique_lock<shared_mutex> lock_;
+  };
+  class ReadAccess
+  {
+  public:
+    ReadAccess(shared_lock<shared_mutex> lock, const V& ref)
+      : lock_{ move(lock) }
+      , ref_to_value{ ref }
+    {}
+
     const V& ref_to_value;
-    lock_guard<mutex> g;
-    ReadAccess(const V& val, mutex& m):g(m), ref_to_value(val){}
+
+  private:
+    shared_lock<shared_mutex> lock_;
   };
 
-  struct Bucket {
-      MapType data;
-      mutex m;
-  };
+  explicit ConcurrentMap(size_t bucket_count)
+    : maps_{ bucket_count }
+  {}
 
-  explicit ConcurrentMap(size_t bucket_count): buckets(bucket_count){
+  WriteAccess operator[](const K& key)
+  {
+    auto& protected_map = get_by_key(key);
+
+    unique_lock lock{ protected_map.mutex };
+    auto& val = protected_map.map[key];
+    return { move(lock), val };
   }
 
-  WriteAccess operator[](const K& key){
-      size_t b = hasher(key) % buckets.size();
-      return WriteAccess(buckets[b].data[key], buckets[b].m);
+  ReadAccess At(const K& key) const
+  {
+    const auto& protected_map = get_by_key(key);
+
+    shared_lock lock{ protected_map.mutex };
+    const auto& val = protected_map.map.at(key);
+    return { move(lock), val };
   }
 
+  bool Has(const K& key) const
+  {
+    const auto& protected_map = get_by_key(key);
 
-  ReadAccess At(const K& key) const{
-      size_t b = hasher(key) % buckets.size();
-      return ReadAccess(buckets[b].data.at(key), buckets[b].m);
+    shared_lock lock{ protected_map.mutex };
+    return protected_map.map.count(key) > 0;
   }
 
-  bool Has(const K& key) const{
-      size_t b = hasher(key) % buckets.size();
-      return buckets[b].data.count(key)>0;
-  }
-
-  MapType BuildOrdinaryMap() const{
-      MapType res;
-      for(const Bucket& b : buckets){
-          for(const auto& mp : b.data){
-              res[mp.first] = mp.second;
-          }
-      }
-      return res;
+  MyType BuildOrdinaryMap() const
+  {
+    MyType res;
+    for (auto& protected_map : maps_) {
+      shared_lock lock{ protected_map.mutex };
+      res.insert(begin(protected_map.map), end(protected_map.map));
+    }
+    return res;
   }
 
 private:
-  Hash hasher;
-  mutable vector<Bucket> buckets;
-};
+  ProtectedMap& get_by_key(const K& key)
+  {
+    const auto idx = Hash{}(key) % maps_.size();
+    return maps_[idx];
+  }
+  const ProtectedMap& get_by_key(const K& key) const
+  {
+    const auto idx = Hash{}(key) % maps_.size();
+    return maps_[idx];
+  }
 
-template <typename K, typename V>
-ostream& operator<<(ostream& out, const unordered_map<K, V>& cmap ){
-    for (const auto& el: cmap){
-        out << el.first << " : " << el.second << '\n';
-    }
-    return out;
-}
+  vector<ProtectedMap> maps_;
+};
 
 
 void RunConcurrentUpdates(
@@ -262,10 +292,10 @@ void TestHas() {
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestConcurrentUpdate);
-//  RUN_TEST(tr, TestReadAndWrite);
-//  RUN_TEST(tr, TestSpeedup);
-//  RUN_TEST(tr, TestConstAccess);
-//  RUN_TEST(tr, TestStringKeys);
-//  RUN_TEST(tr, TestUserType);
-//  RUN_TEST(tr, TestHas);
+  RUN_TEST(tr, TestReadAndWrite);
+  RUN_TEST(tr, TestSpeedup);
+  RUN_TEST(tr, TestConstAccess);
+  RUN_TEST(tr, TestStringKeys);
+  RUN_TEST(tr, TestUserType);
+  RUN_TEST(tr, TestHas);
 }
