94931cf53975a39bb865901b9c5af18882445f10
bogdan <lakatosh@phystech.edu>
2020-05-10 00:04:08 +0300

    passed solution

--- 26_domains/domains_bad_wa.cpp
+++ 26_domains/domains_bad_wa.cpp
@@ -3,136 +3,71 @@
 #include <string>
 #include <string_view>
 #include <vector>
-#include <set>
 #include <string_view>
-#include "test_runner.h"
+#include <unordered_map>
+
 using namespace std;
-
-
-/*
-bool IsSubdomain(string_view subdomain, string_view domain) {
-  auto i = subdomain.size() - 1;
-  auto j = domain.size() - 1;
-  while (i >= 0 && j >= 0) {
-    if (subdomain[i--] != domain[j--]) {
-      return false;
-    }
-  }
-  return (i < 0 && domain[j] == '.')
-      || (j < 0 && subdomain[i] == '.');
-}
-
-
-vector<string> ReadDomains() {
-  size_t count;
-  cin >> count;
-
-  vector<string> domains;
-  for (size_t i = 0; i < count; ++i) {
-    string domain;
-    getline(cin, domain);
-    domains.push_back(domain);
-  }
-  return domains;
-}
-*/
-
 
 class DomainFilter{
 private:
-    set<string> banned_;
-    size_t max_size_;
+    unordered_map<string_view, vector<string_view>> data_;     //domain -> subdomains
+
+    string_view GetMainDomain(const string_view address) const {
+        size_t pos = address.find_last_of('.');
+        if(pos==string::npos){return address;}
+        else{return address.substr(pos+1, string::npos);}
+    }
+
 public:
-    DomainFilter(vector<string> given_dommains):max_size_(0){
-        for(auto el : given_dommains){
-            reverse(el.begin(), el.end());
-            banned_.insert(el);
-            if(el.size()>max_size_){max_size_=el.size();}
+    DomainFilter(const vector<string>& given_dommains){
+        for(const auto& el : given_dommains){
+            string_view main = GetMainDomain(el);
+            data_[main].push_back(el);
         }
     }
 
-    set<string> GetBanned() const {return banned_; }
-    size_t GetMaxSubNum() const {return max_size_;}
-
-    bool CompareReverseParts(const string& banned_dom, const string& cur_dom) const {
-        if(banned_dom.size()==cur_dom.size()){
-            return banned_dom==cur_dom;
-        }
-        bool res = banned_dom==cur_dom.substr(0, banned_dom.size());
-        return res && cur_dom[banned_dom.size()]=='.';
+    bool CheckDomainBelonging(string_view domain, string_view address) const {
+        if(address.size()<domain.size()){return false;}
+        if(address.size()==domain.size()){return domain==address;}
+        size_t pos = address.size()-domain.size()-1;
+        string_view check = address.substr(pos, string::npos);
+        return check.front()=='.' && domain==check.substr(1, string::npos);
     }
 
-    bool CheckForGood(const string& domain) const {
-        if (banned_.empty()){return true;}
-        size_t pos;
-        if(domain.size()<max_size_+1){pos = 0;}
-        else{pos = domain.size() - max_size_-1;}
-        string check = domain.substr(pos, string::npos);
-        reverse(check.begin(), check.end());
-        auto up_it = banned_.upper_bound(check);
-        for(auto it = banned_.begin(); it!=up_it; it++){
-            if (CompareReverseParts(*it, check)){
+    bool CheckForGood(const string_view domain) const {
+        if(data_.empty()){return true;}
+        string_view main = GetMainDomain(domain);
+        if(data_.count(main)==0){return true;}
+        for(const string_view el : data_.at(main)){
+            if(CheckDomainBelonging(el, domain)){
                 return false;
             }
         }
         return true;
     }
+
 };
 
-
-
 int main() {
-    /*
-    vector<string> banned_domains = {"ya.ru", "maps.me", "m.ya.ru", "com"};
-    DomainFilter filter(move(banned_domains));
-    //vector<string> check = {"ya.ru", "ya.com", "m.maps.me", "moscow.m.ya.ru", "maps.com", "maps.ru", "ya.ya"};
-    vector<string> check = {"ya.ya"};
-    for(auto& el : check){
-        cout << filter.CheckForGood(el);
-    }*/
-
     size_t count;
     cin >> count;
-    vector<string> banned_domains(count ,string());
+    vector<string> banned_domains;
+    banned_domains.reserve(count);
     for(size_t i=0; i<count; i++){
-        cin >> banned_domains[i];
+        string tmp;
+        cin >> tmp;
+        banned_domains.push_back(move(tmp));
     }
-    DomainFilter filter(move(banned_domains));
+    DomainFilter filter(banned_domains);
     cin >> count;
     for(size_t i=0; i<count; i++){
-        string domain;
-        cin >> domain;
-        if(filter.CheckForGood(domain)){
+        string address;
+        cin >> address;
+        if (filter.CheckForGood(address)){
             cout << "Good\n";
         } else {
             cout << "Bad\n";
         }
     }
-
-    /*
-  const vector<string> banned_domains = ReadDomains();
-  const vector<string> domains_to_check = ReadDomains();
-
-  for (string_view domain : banned_domains) {
-    reverse(begin(domain), end(domain));
-  }
-  sort(begin(banned_domains), end(banned_domains));
-
-  size_t insert_pos = 0;
-  for (string& domain : banned_domains) {
-    if (insert_pos == 0 || !IsSubdomain(domain, banned_domains[insert_pos - 1])) {
-      swap(banned_domains[insert_pos++], domain);
-    }
-  }
-  banned_domains.resize(insert_pos);
-
-  for (const string_view domain : domains_to_check) {
-    if (const auto it = upper_bound(begin(banned_domains), end(banned_domains), domain);
-        it != begin(banned_domains) && IsSubdomain(domain, *prev(it))) {
-      cout << "Good" << endl;
-    } else {
-      cout << "Bad" << endl;
-    }
-  }*/
   return 0;
 }
